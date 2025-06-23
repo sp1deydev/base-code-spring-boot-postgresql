6a30958b09ec2a8893d7f7bd36be64b2d78bfacf
thientdk <duckhanhthientran@gmail.com>
2025-06-23 11:36:39 +0700

    initial commit

---
+++ src/main/java/com/thientdk/base_code_postgresql/BaseCodePostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.thientdk.base_code_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseCodePostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseCodePostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thientdk/base_code_postgresql/BaseCodePostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thientdk.base_code_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseCodePostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
